87f9505700ede26a4bd8d0a9094525bb18b793b2
richard-dev <richardl890@gmail.com>
2017-03-19 19:14:55 -0400

    Add QueryUtils - extract JSON Objects. Modify EarthquakeActivity to call QueryUtil instead of static data.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,16 +31,9 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create earthquake arraylist
-        ArrayList<Earthquake> earthquake = new ArrayList<>();
-        // Create a fake list of earthquake locations.
-        earthquake.add(new Earthquake("7.2","San Francisco","Feb 2, 2016"));
-        earthquake.add(new Earthquake("6.1","London","July 20, 2015"));
-        earthquake.add(new Earthquake("3.9","Tokyo","Nov 10, 2014"));
-        earthquake.add(new Earthquake("5.4","Mexico City","May 3, 2014"));
-        earthquake.add(new Earthquake("2.8","Moscow","Jan 31, 2013"));
-        earthquake.add(new Earthquake("4.9","Rio de Janeiro","Aug 19, 2012"));
-        earthquake.add(new Earthquake("1.6","Paris","Oct 30, 2011"));
+        // Create earthquake arraylist from QueryUtils which extracts data from a JSON object
+        // and puts it in an EarthQuake ArrayList.
+        ArrayList<Earthquake> earthquake = QueryUtils.extractEarthquakes();
 
         // Create a new {@link ArrayAdapter} of earthquakes
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquake);
