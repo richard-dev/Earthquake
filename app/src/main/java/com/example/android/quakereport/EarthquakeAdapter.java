15e64586523f859e877969493e31593535398add
richard-dev <richardl890@gmail.com>
2017-03-20 09:02:46 -0400

    Add color values. Add magnitude circle drawable. Add magnitude circles to list_item.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,9 +1,11 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.PluralsRes;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,7 +43,49 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         DecimalFormat decimalFormat = new DecimalFormat("0.0");
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_textview);
         magnitudeTextView.setText(decimalFormat.format(currentEarthquake.getMagnitude()));
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(currentEarthquake.getMagnitude());
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+        }
+        // You can call ContextCompat getColor() to convert the color resource ID into an
+        // actual integer color value, and return the result as the return value of the
+        // getMagnitudeColor() helper method.
+        magnitudeCircle.setColor(ContextCompat.getColor(getContext(), magnitudeColorResourceId));
 
+        // City
         // String to split
         String city = currentEarthquake.getCity();
         String cityArray[] = city.split("of ");
